16b8231134e8982627e71ea3fd9a44acc858c465
Andrew Kim <andiskim@gmail.com>
2022-05-25 17:10:55 +0300

    first commit

---
+++ styles/theme.ts
@@ -0,0 +1,62 @@
+// import { extendTheme } from "@chakra-ui/react";
+
+// const appTheme = extendTheme({
+//   colors: {
+//     primary: {
+//       900: "#094BA9",
+//       800: "#1169C8",
+//       700: "#147BDA",
+//     },
+//     red: "#DF5954",
+//     green: "#54DF73",
+//     neutral: {
+//       900: "#161616",
+//       800: "#363636",
+//       700: "#545454",
+//       600: "#676767",
+//       500: "#8F8F8F",
+//       400: "#B0B0B0",
+//       300: "#D4D4D4",
+//       200: "#E5E5E5",
+//       100: "#F0F0F0",
+//       50: "#F8F8F8",
+//     },
+//     block: "#292929",
+//     background: {
+//       start: "#272727",
+//       end: "#1B1B1B",
+//     },
+//   },
+//   fonts: {
+//     app: "'Prompt', sans-serif",
+//     landing: "'Prompt', sans-serif",
+//   },
+//   fontSizes: {
+//     12: "12px",
+//     14: "14px",
+//     16: "16px",
+//     18: "18px",
+//     24: "24px",
+//     28: "28px",
+//     32: "32px",
+//     40: "40px",
+//   },
+//   fontWeights: {
+//     light: 300,
+//     regular: 400,
+//     medium: 500,
+//   },
+//   lineHeights: {
+//     normal: "150%",
+//     bigger: "175%",
+//     big: "200%",
+//   },
+//   breakpoints: {
+//     sm: "576px",
+//     md: "768px",
+//     lg: "992px",
+//     xl: "1200px",
+//     xxl: "1400px",
+//   },
+// });
+// export default appTheme;
